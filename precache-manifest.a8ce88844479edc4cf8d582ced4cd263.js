7aae8d1eb54a14895da41fa42aa1ba9371858abd
Roberto Samia <bojosteph@gmail.com>
2019-09-13 19:17:16 -0400

    Updates

---
+++ precache-manifest.a8ce88844479edc4cf8d582ced4cd263.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff9276b2311848426c2673a98488bd2",
+    "url": "/articles-pages/index.html"
+  },
+  {
+    "revision": "5c127d45254dfe1f1df1",
+    "url": "/articles-pages/static/css/main.d23a2bb2.chunk.css"
+  },
+  {
+    "revision": "71eee244b4db2a38b01d",
+    "url": "/articles-pages/static/js/2.44e2a378.chunk.js"
+  },
+  {
+    "revision": "5c127d45254dfe1f1df1",
+    "url": "/articles-pages/static/js/main.9d17ba6f.chunk.js"
+  },
+  {
+    "revision": "0f891f031066136aeeb4",
+    "url": "/articles-pages/static/js/runtime~main.c3ec9e08.js"
+  }
+]);
